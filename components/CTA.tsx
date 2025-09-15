747006c2a15b4d33880d9d54f464b5d6db2fc443
Huzaifa Karim <karimhuzaifa590@gmail.com>
2025-09-15 16:56:04 +0500

    Refactor CTA button to use Link component

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -13,12 +13,10 @@ const Cta = () => {
         voice conversations that feel natural and fun.
       </p>
       <Image src="images/cta.svg" alt="cta" width={362} height={232} />
-      <button className="btn-primary">
+      <Link href="/companions/new" className="btn-primary">
         <Image src="/icons/plus.svg" alt="plus" width={12} height={12} />
-        <Link href="/companions/new">
-          <p>Build a New Companion</p>
-        </Link>
-      </button>
+        <p>Build a New Companion</p>
+      </Link>
     </section>
   );
 };
